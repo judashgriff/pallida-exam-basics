d2b6fd3e3013d163b1e34d91b16980ff52a1e843
András Krausz <Andras.Krausz@gmail.com>
2017-10-06 09:20:11 +0200

    Feat(unique_chars.py): function created to separate unique chars.

---
+++ unique_chars.py
@@ -0,0 +1,15 @@
+# Create a function called `unique_characters` that takes a string as parameter
+# and returns a list with the unique letters of the given string
+# Create basic unit tests for it with at least 3 different test cases
+# print(unique_characters("anagram"))
+# Should print out:
+# ["n", "g", "r", "m"]
+
+def unique_characters(string):
+    unique_chars_list = []
+    for letter in string:
+        if letter not in unique_chars_list:
+            unique_chars_list.append(letter)
+    return unique_chars_list
+
+print(unique_characters("anagram"))
